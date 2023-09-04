1f7576b4ca154d54e8456e79db9d8a97383824ec
chiefnaheem <naheemademola21@gmail.com>
2023-09-04 03:47:27 -0700

    rendered deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuickHttpService } from './quick-http.service';
 
 @Module({
   imports: [
-    HttpModule
+    HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '../build'),   // <-- path to the static files
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, QuickHttpService],
--- src/serverless.ts
+++ src/serverless.ts
@@ -2,12 +2,17 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import serverlessExpress from "@vendia/serverless-express";
 import { Callback, Context, Handler } from "aws-lambda";
+import express from "express";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 let server: Handler;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.init();
+  // app.use(express.static('build'));;
+  app.useStaticAssets(join(__dirname, 'build'))
 
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
